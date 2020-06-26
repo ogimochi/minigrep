af7ed2e6bce5f438407281603600fabd7644d3cb
mochidan <momochidan@gmail.com>
2020-06-26 16:00:38 +0900

    クレートに分けたところまで。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate minigrep;
+
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // 引数解析時に問題
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    // 第一引数は、grep のクエリ
+    println!("Searching for {}", config.query);
+    // 第二引数は、grepするファイル名
+    println!("It file {}", config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
