270dc2b56423c9cec0b66ea41a9a3ec4b08f7569
mochidan <momochidan@gmail.com>
2020-07-01 13:55:29 +0900

    完成。

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::error::Error;
+use std::env;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -16,16 +18,87 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Ok(Config{ query, filename})
+        // CASE_INSENSITIVEに何も設定されていない場合は、Errが返却される。
+        // 何か設定されている場合は、ON。何も設定されていない場合は、OFF。
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config{query, filename, case_sensitive})
     }
 }
 
-pub fn run(config: Config) -> Result<(), Box<Error>>{
+pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let mut f = File::open(config.filename)?;
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    println!("With text:\n{}", contents);
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line)
+    }
+
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+sage, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(
+            vec!["sage, fast, productive."],
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+sage, fast, productive.
+Pick three.
+Trust me";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me"],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,12 @@ fn main() {
 
     let config = Config::new(&args).unwrap_or_else(|err| {
         // 引数解析時に問題
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    // 第一引数は、grep のクエリ
-    println!("Searching for {}", config.query);
-    // 第二引数は、grepするファイル名
-    println!("It file {}", config.filename);
-
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
